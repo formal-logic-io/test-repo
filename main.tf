68164fac38efabe44bd2bfd3f968958cd52eef14
Deepak Dharan Padmini <dharanpdeepak@users.noreply.github.com>
2024-01-19 07:06:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,34 @@
-provider "aws" {
-  region = "ap-south-1"
+
+terraform {
+ required_providers {
+   aws = {
+     source  = "hashicorp/aws"
+     #version = "~> 4.19.0"
+   }
+ }
 }
 
-resource "aws_instance" "example" {
-  ami           =  "ami-0fb653ca2d3203ac1" 
-  instance_type = "t2.micro"
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
